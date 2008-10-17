65b10b744c64b845bcfb9bfac6b531a2a0e8ab8d
Adam Wiggins <adam@heroku.com>
2008-10-17 03:14:07 -0700

    post page

--- main.rb
+++ main.rb
@@ -6,13 +6,20 @@ DB = Sequel.sqlite
 DB.create_table :posts do
 	column :title, :text
 	column :body, :text
+	column :slug, :text
 	column :created_at, :timestamp
 end
 
-DB[:posts] << { :title => "Erlang", :body => "Lorum ipsum", :created_at => Time.now }
-DB[:posts] << { :title => "DDL Transactions", :body => "Lorum ipsum 2", :created_at => Time.now }
+DB[:posts] << { :title => "Erlang", :body => "Lorum ipsum", :created_at => Time.now, :slug => "1-erlang" }
+DB[:posts] << { :title => "DDL Transactions", :body => "Lorum ipsum 2", :created_at => Time.now, :slug => "2-ddl-transactions" }
 
 get '/' do
 	erb :index, :locals => { :posts => DB[:posts] }
+end
+
+get '/*:slug' do
+	post = DB[:posts].filter(:slug => params[:slug]).first
+	stop [ 404, "Page not found" ] unless post
+	erb :post, :locals => { :post => post }
 end
 
