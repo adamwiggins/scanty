8b57157ef0f165741882620beb04de31ffe4cf01
Adam Wiggins <adam@heroku.com>
2008-10-27 00:00:39 -0700

    use a layout

--- main.rb
+++ main.rb
@@ -7,15 +7,17 @@ DB = Sequel.connect('sqlite://blog.db')
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/lib')
 require 'post'
 
+layout 'layout'
+
 get '/' do
 	posts = Post.reverse_order(:created_at).limit(10)
-	erb :index, :locals => { :posts => posts }
+	erb :index, :locals => { :posts => posts }, :layout => false
 end
 
 get '/past/:year/:month/:day/:slug/' do
 	post = Post.filter(:slug => params[:slug]).first
 	stop [ 404, "Page not found" ] unless post
-	erb :post, :locals => { :post => post }
+	erb :post, :locals => { :post => post, :title => post.slug }
 end
 
 get '/past/:year/:month/:day/:slug' do
@@ -24,12 +26,13 @@ end
 
 get '/past' do
 	posts = Post.reverse_order(:created_at)
-	erb :archive, :locals => { :posts => posts }
+	erb :archive, :locals => { :posts => posts, :title => "Archive" }
 end
 
 get '/past/tags/:tag' do
-	posts = Post.filter(:tags.like("%#{params[:tag]}%")).reverse_order(:created_at).limit(30)
-	erb :index, :locals => { :posts => posts }
+	tag = params[:tag]
+	posts = Post.filter(:tags.like("%#{tag}%")).reverse_order(:created_at).limit(30)
+	erb :tagged, :locals => { :posts => posts, :title => "Posts tagged #{tag}", :tag => tag }
 end
 
 get '/feed' do
